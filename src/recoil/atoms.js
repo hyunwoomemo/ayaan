b2cbbcd912cf70cd152cf4eae53f39e2a771c2fc
hyunwoomemo <archihw94@gmail.com>
2023-07-10 00:02:45 +0900

    전역 변수 recoil 활용

---
+++ src/recoil/atoms.js
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+export const projectState = atom({
+  key: 'projectState',
+  default: 0,
+})
+
+export const contactState = atom({
+  key: 'contactState',
+  default: 0,
+})
