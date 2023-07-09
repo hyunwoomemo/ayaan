1ee3100ae6305bcad565818c36cccbcf06678b3e
hyunwoomemo <archihw94@gmail.com>
2023-07-09 23:58:35 +0900

    Recoil 정의 router 정의 글로벌스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Main from './pages/Main';
+import GlobalStyle from './components/common/GlobalStyle';
+import { RecoilRoot } from 'recoil';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+        </Routes>
+      </Router>
+    </RecoilRoot>
   );
 }
 
